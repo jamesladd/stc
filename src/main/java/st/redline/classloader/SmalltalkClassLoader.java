e11e6ae06305024349911e2ddd5f03aafd150db3
James Ladd <james_ladd@hotmail.com>
2013-12-27 16:14:15 +1100

    Moved examples into own package and added cache of loaded classes to classloader.

--- src/main/java/st/redline/classloader/SmalltalkClassLoader.java
+++ src/main/java/st/redline/classloader/SmalltalkClassLoader.java
@@ -2,20 +2,37 @@ package st.redline.classloader;
 
 import st.redline.compiler.Compiler;
 
+import java.util.HashMap;
+
 public class SmalltalkClassLoader extends ClassLoader {
 
     private final SourceFinder sourceFinder;
+    private final HashMap<String, Class> cache;
 
     public SmalltalkClassLoader(ClassLoader classLoader, SourceFinder sourceFinder) {
         super(classLoader);
         this.sourceFinder = sourceFinder;
+        this.cache = new HashMap<String, Class>();
     }
 
     public Class findClass(String name) throws ClassNotFoundException {
+        Class cls = cachedClass(name);
+        if (cls != null)
+            return cls;
         byte[] classData = loadClassData(name);
         if (classData == null)
             return super.findClass(name);
-        return defineClass(null, classData, 0, classData.length);
+        cls = defineClass(null, classData, 0, classData.length);
+        cacheClass(cls, name);
+        return cls;
+    }
+
+    private void cacheClass(Class cls, String name) {
+        cache.put(name, cls);
+    }
+
+    private Class cachedClass(String name) {
+        return cache.get(name);
     }
 
     private byte[] loadClassData(String name) {

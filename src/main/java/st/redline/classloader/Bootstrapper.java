716fececa959cd3fdde1e67272ebcfcfad24d324
James Ladd <james.ladd@mywave.me>
2016-12-01 07:52:55 +1100

    Created a PrimObject to be the Java backing for all Smalltalk Objects.

--- src/main/java/st/redline/classloader/Bootstrapper.java
+++ src/main/java/st/redline/classloader/Bootstrapper.java
@@ -6,5 +6,14 @@ public class Bootstrapper {
 
     public void bootstrap(SmalltalkClassLoader smalltalkClassLoader) {
         smalltalkClassLoader.cacheObject("st.redline.core.PrimObject", new PrimObject());
+        loadProtoObject(smalltalkClassLoader);
+    }
+
+    private void loadProtoObject(ClassLoader classLoader) {
+        try {
+            classLoader.loadClass("st.redline.core.ProtoObject").newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
